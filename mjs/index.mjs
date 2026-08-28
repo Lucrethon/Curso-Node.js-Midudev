11a76c8bb52d29df972556b314fd057dde3d71d2
Lucrethon <lucrecimich@gmail.com>
2026-08-28 14:49:52 -0400

    Learning ES Export Modules

---
+++ mjs/index.mjs
@@ -0,0 +1,9 @@
+// .js -> por defecto utiliza CommonJS
+// .mjs -> para utilizar ES Modules
+// .cjs -> Para forzar utilizar CommonJS
+
+// con el mjs podemos utilizar el import comun 
+
+import { sumar } from "./suma.mjs"
+
+sumar(5, 3)
