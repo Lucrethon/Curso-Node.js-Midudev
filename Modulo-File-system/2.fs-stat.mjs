23d73885b1dbbcb8a88d11517b67a31f1b3395b6
Lucrethon <lucrecimich@gmail.com>
2026-08-31 19:13:05 -0400

    Add file system statistics module and example text file

---
+++ Modulo-File-system/2.fs-stat.mjs
@@ -0,0 +1,12 @@
+
+// fs = filesystem
+import fs from 'node:fs'
+
+const stats = fs.statSync('./archivo.txt')
+
+console.log(
+    stats.isFile(), // si es un fichero
+    stats.isDirectory(), // si es un directorio
+    stats.isSymbolicLink(), // si es un enlace simbolico
+    stats.size, // tamaño en bytes 
+)
