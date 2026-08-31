31afcbb611cd6158208ef7ed72a067fc513043a2
Lucrethon <lucrecimich@gmail.com>
2026-08-31 19:46:23 -0400

    Add synchronous and asynchronous file reading examples with two text files

---
+++ Modulo-File-system/3.fs-readFile.mjs
@@ -0,0 +1,36 @@
+import fs from 'node:fs'
+
+const RUTAS = {
+    texto1 : './archivo.txt',
+    texto2 : './archivo-2.txt'
+}
+
+// sincrono 
+
+console.log('Leyendo el primer archivo...')
+const text = fs.readFileSync(RUTAS.texto1, 'utf-8')
+console.log(text)
+
+console.log('Hacer algo mientras lee el archivo...')
+
+console.log('Leyendo el segundo archivo...')
+const secondText = fs.readFileSync(RUTAS.texto2, 'utf-8')
+console.log(secondText)
+
+// asincrono 
+
+
+fs.readFile(RUTAS.texto1, 'utf-8', (err, text) => {
+// mientras se ejecuta lo que esta en el callback
+    if (err) console.log(err.text())
+    else console.log(text)
+})
+// se va ejecutando esto y no espera a que el callback termine 
+// esto se ejecuta antes de que se lea el texto porque es asincrono
+console.log('Hacer algo mientras lee el archivo...')
+
+fs.readFile(RUTAS.texto2, 'utf-8', (err, text) => {
+// mientras se ejecuta lo que esta en el callback
+    if (err) console.log(err.text())
+    else console.log(text)
+})
