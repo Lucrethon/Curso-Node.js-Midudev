6843c0f3e7d2b4df37ee0c0eb9b6378e16945e5a
Lucrethon <lucrecimich@gmail.com>
2026-09-01 10:24:50 -0400

    Refactor file reading examples to clarify sequential execution with await

--- Modulo-File-system/4.fs-async-await-secuencial.mjs
+++ Modulo-File-system/4.fs-async-await-secuencial.mjs
@@ -6,14 +6,24 @@ const RUTAS = {
 };
 
 
-// con await: 
+// con await (secuencial): 
 
 // esto solo funciona con ES Modules
 // con common modules no funciona 
-const textAwait = await fs.readFile(RUTAS.texto2, 'utf-8');
+
+console.log('Leyendo primer archivo...')
+const textAwait = await fs.readFile(RUTAS.texto1, 'utf-8');
 console.log('con await (solo mjs)', textAwait);
 
-// con async - await secuencial : 
+console.log('Haciendo cosas mientras lee el archivo...')
+
+console.log('Leyendo segundo archivo...')
+const textAwait2 = await fs.readFile(RUTAS.texto2, 'utf-8');
+console.log('con await (solo mjs)', textAwait2);
+
+
+
+// con async - await (secuencial) : 
 
 // 1ra forma: funcion auto-invocada 
 // IIFE - Inmediatly Invoked Function Expression 
@@ -23,3 +33,7 @@ console.log('con await (solo mjs)', textAwait);
         console.log('con async-await', textAwait)
     }
 )() // estamos invocando una funcion al mismo tiempo que la estamos ejecutando 
+
+
+// es secuencial porque hasta que no se termine de leer un archivo, no lee el otro y no ejecuta la lectura
+// en paralelo, los textos se leen paralelamente 
