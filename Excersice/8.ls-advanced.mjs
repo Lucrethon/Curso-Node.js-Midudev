0eaaed01d06b637655c038c0d4e6cfcfaf04a5d6
Lucrethon <lucrecimich@gmail.com>
2026-09-01 18:55:26 -0400

    Refactor ls-advanced script to use async/await for directory and file reading, adding error handling for both operations

--- Excersice/8.ls-advanced.mjs
+++ Excersice/8.ls-advanced.mjs
@@ -1,27 +1,67 @@
 import fs from 'node:fs/promises'
+import path from 'node:path';
 
 const folder = process.argv[2] ?? '.'
 // posicion 0: ruta absoluta al ejecutable de Node.js en el sistema
 // posición 1: ruta absoluta al archivo JavaScript que se está ejecutando.
 // posición 2: Cualquier argumento personalizado que tú le pases en la terminal.
+// TODO lo devuelve en string 
 
-console.log('carpeta a analizar:', folder)
+async function ls(folder) {
+    console.log('carpeta a analizar:', folder)
+    let files
 
-fs.readdir(folder)
-.then(files => {
-    files.forEach(file => {
-    console.log(file) 
-    })
-})
-.catch(err => {
-    if (err) {
-    console.log('Error al leer el directorio', err)
-    return
+    try {
+        files = await fs.readdir(folder)
     }
-});
+    catch {
+        if (err) {
+        console.log('Error al leer el directorio', err)
+        process.exit(1)
+    }}
+
+}
+
+
+const filesPromises = files.map(async (file) => {
+    const filePath = path.join(folder, file)
+    let stats
+    try {
+        stats = await fs.stat(filePath) // información del archivo 
+    }
+    catch {
+        if (err) {
+            console.log('Error al leer el archivo')
+            process.exit(1)
+        }
+    }
+
+    // const isFile = stats.isFile() // si es un fichero
+    const isDirectory = stats.isDirectory() // si es un directorio
+    const fileType = isDirectory ? 'd' : '-'
+    const fileSize = stats.size // tamaño en bytes 
+    const fileModified = stats.mtime.toLocaleString()
+
+    return `${fileType} ${file.padEnd(40)} ${fileSize.toString().padStart(10)} ${fileModified}`
+})
+
+
+
+const filesInfo = await Promise.all(filesPromises) // esto se hace porque estamos mapeando varios archivos y creando varias promesas 
+filesInfo.forEach(fileInfo => console.log(fileInfo))
+
+
+
+// se tienen que hacer dos try-catch porque son procesos diferentes: 
+// 1. el try-catch para leer el directorio
+// 2. el try-catch para leer en paralelo la información de los archivos 
+// tambien se pueden separar los procesos en dos funciones diferentes
+
 
 // ahora en el terminal al yo ejecutar este script, puedo pasarle el nombre de la carpeta que quiero analizar despues del comando de ejecución de script y me va a devolver los ficheros de ese directorio
 
 // node 8.ls-advanced.mjs ../Modulo-File-system // <-- analizara la carpeta Modulo-File-system
 
 // si el directorio no existe, se maneja el error en el callback con el err 
+
+ls(folder)
