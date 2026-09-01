890e206e90345bd4220a4c404fbd08c15172ee46
Lucrethon <lucrecimich@gmail.com>
2026-09-01 18:40:13 -0400

    Add explanation comment for fs.statSync usage in file metadata retrieval

--- Modulo-File-system/2.fs-stat.mjs
+++ Modulo-File-system/2.fs-stat.mjs
@@ -3,6 +3,7 @@
 import fs from 'node:fs'
 
 const stats = fs.statSync('./archivo.txt')
+// fs.stat() en Node.js consulta y devuelve los metadatos de un archivo o directorio (como su tamaño, permisos, fechas de creación/modificación o si es un archivo o carpeta).
 
 console.log(
     stats.isFile(), // si es un fichero
