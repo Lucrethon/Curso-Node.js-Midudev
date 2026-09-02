db3c22d606ea69f0dc9bd39131392689afc69c41
Lucrethon <lucrecimich@gmail.com>
2026-09-02 19:37:01 -0400

    feat: use environment variable for desired port in HTTP server

--- Modulo-http/9.http.mjs
+++ Modulo-http/9.http.mjs
@@ -1,7 +1,7 @@
 import http from 'node:http'
 import { findAvilablePort } from './10.free-port.mjs'
 
-const desiredPort = 3001
+const desiredPort = process.env.PORT ?? 3001
 
 const server = http.createServer((request, response) => {
     console.log('request received')
@@ -19,3 +19,6 @@ findAvilablePort(desiredPort).then(port => {
 // si el puerto esta en uso, se le puede pasar el puerto 0 
 // el puerto 0 lo que hace es que busca automaticamente el primer puerto que este disponible 
 // y se recupera con server.address().port
+
+// si se quiere hacer que el puerto sea una variable de entorno
+// antes de ejecutar el script, se tiene que declarar la variable (PORT=1234 node 9.http.mjs)
