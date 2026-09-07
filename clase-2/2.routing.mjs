b93177bd059f75acdcdcc3190643743ed237a34c
Lucrethon <lucrecimich@gmail.com>
2026-09-07 15:48:17 -0400

    feat: implement POST request handling for Pokémon data submission

--- clase-2/2.routing.mjs
+++ clase-2/2.routing.mjs
@@ -25,9 +25,43 @@ const processRequest = (req, res) => {
         case 'POST': {
             
             switch(url) {
-                case '/pokemon':{
+                case '/pokemon': {
                     let body = ''
+                    // en la request se envia una información (body)
+                    // como se recibe en el servidor?
+                    // escuchando el evento data
+                    // hay que pensar el envio de información como una tubería: va llegando información
+                    // a medida que va llegando la información, se va a querer ir un trozo de lo que va llegando:
+                    req.on('data', chunk => {
+                        body += chunk.toString()
+                        })
+                        // a medida que va llegando información, se va guardando en el body 
+                        // se convierte de binario a string 
+                    // node.js se basa en eventos, entonces una vez que se haya terminado de enviar todos los datos, escuchamos el evento 'end'
+                    req.on('end', () => {
+                        const data = JSON.parse(body)
+                        
+                        // convertimos todo el string a JSON
+                        // Aqui pudieramos hacer muchas cosas:
+                        // llamar a una base de datos para guardar la info
+
+                        // para comprobar que todo funcione correctamente, se envian los mismos datos: 
+                        res.writeHead(201, {'Content-Type': 'application/json; charset=utf-8'})
+
+                        // data.timestamp = Date.now()
+
+                        res.end(JSON.stringify(data)) // esto es para comprobar que lo mismo que se envio es lo mismo que recibimos 
+
+                        // The HTTP 201 Created success status response code indicates that the request has succeeded and has led to the creation of a resource.
+                        })
+                        break
                 }
+
+                default: {
+                    res.statusCode = 404;
+                    res.setHeader('Content-Type', 'text/plain; charset=utf-8')
+                    return res.end('Not Found 404')
+                    }
             }
         }
     }
