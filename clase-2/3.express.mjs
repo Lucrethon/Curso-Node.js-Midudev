aa2715dc2f6c29787e2da11c8cb2deacf142420a
Lucrethon <lucrecimich@gmail.com>
2026-09-07 16:08:28 -0400

    ensure JSON response for /json endpoint

--- clase-2/3.express.mjs
+++ clase-2/3.express.mjs
@@ -4,8 +4,14 @@ const app = express()
 const PORT = process.env.PORT ?? 1234
 
 //aqui estamos diciendo: cuando se haga una peticion (req) "GET" en la url "/", envia este body 
+// Express en la mayoria de los casos va a determinar autimaticamente el content type 
 app.get('/', (req, res) => {
-    res.status(200).send('<h1>Mi pagina</h1>')
+    res.send('<h1>Mi pagina</h1>')
+})
+
+// para enviar un json: 
+app.get('/json', (req, res) => {
+    res.json({"message" : "Hola Mundo"})
 })
 
 app.listen(PORT, () => {
