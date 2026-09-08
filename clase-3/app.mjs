a8eb52217894355b903c9b9d538df0e68caf65e6
Lucrethon <lucrecimich@gmail.com>
2026-09-08 10:47:21 -0400

    fix: add default 404 error handling middleware

--- clase-3/app.mjs
+++ clase-3/app.mjs
@@ -10,6 +10,12 @@ app.get('/', (req, res) => {
     res.send('<h1>Mi Pagina</h1>')
 })
 
+// Default error 404
+app.use((req, res) => {
+    res.status(404).send('<h1>Error 404</h1>')
+})
+
+// Listen port
 app.listen(PORT, () => {
     console.log(`Sever listening in port http://localhost:${PORT}`)
 })
